f053d67e62ad59f03a0f716ecd1a040afc8210ca
Steven Pritchard <steven.pritchard@gmail.com>
2023-11-29 13:51:43 -0600

    Changes to demonstrate https://github.com/puppetlabs/pdk-templates/issues/543

--- spec/classes/issue_543_spec.rb
+++ spec/classes/issue_543_spec.rb
@@ -5,6 +5,13 @@ require 'spec_helper'
 describe 'issue_543' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:pre_condition) do
+        <<~END
+          if $facts['networking']['fqdn'].empty {
+            fail("No fqdn fact: networking => ${facts['networking']}")
+          }
+        END
+      end
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
